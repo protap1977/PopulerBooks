ec153e62be3dd852a328aaf013c5176b46825178
protap1977 <pk177sf@gmail.com>
2023-03-26 22:47:00 +0600

    this is my 2nd commit

--- app/src/main/java/adhikariit/blogspot/ebookapp/MainActivity.java
+++ app/src/main/java/adhikariit/blogspot/ebookapp/MainActivity.java
@@ -3,6 +3,7 @@ package adhikariit.blogspot.ebookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getData();
+    }
+
+    public void getData(){
+        Toast.makeText(this, "this is testing for me!!", Toast.LENGTH_SHORT).show();
     }
 }
